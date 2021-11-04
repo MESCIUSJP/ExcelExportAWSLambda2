460882d2a2cbb9d8d50d29299644df81ab42e68e
Kuni <kuni.ujiie@grapecity.com>
2021-11-04 14:53:41 +0900

    プロジェクト ファイルを追加します。

---
+++ ExcelExportAWSLambda2/Function.cs
@@ -0,0 +1,81 @@
+using Amazon;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.S3;
+using Amazon.S3.Model;
+using GrapeCity.Documents.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace ExcelExportAWSLambda2
+{
+    public class Function
+    {
+
+        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
+        {
+            APIGatewayProxyResponse response;
+
+            try
+            {
+                // クエリ文字列を取得
+                string queryString;
+                input.QueryStringParameters.TryGetValue("name", out queryString);
+
+                // ワークシートに追加するテキスト
+                string Message = string.IsNullOrEmpty(queryString)
+                    ? "Hello, World!!"
+                    : $"Hello, {queryString}!!";
+
+                //Workbook.SetLicenseKey("");
+
+                Workbook workbook = new Workbook();
+                workbook.Worksheets[0].Range["A1"].Value = Message;
+
+                using (var ms = new MemoryStream())
+                {
+                    workbook.Save(ms, SaveFileFormat.Xlsx);
+
+                    // S3にアップロード
+                    AmazonS3Client client = new AmazonS3Client(RegionEndpoint.APNortheast1);
+                    var request = new PutObjectRequest
+                    {
+                        BucketName = "diodocs-export",
+                        Key = "Result.xlsx",
+                        InputStream = ms
+                    };
+
+                    await client.PutObjectAsync(request);
+                }
+
+                response = new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.OK,
+                    Body = "ファイルが保存されました。",
+                    Headers = new Dictionary<string, string> {
+                        { "Content-Type", "text/plain; charset=utf-8" }
+                    }
+                };
+            }
+            catch (Exception e)
+            {
+                response = new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError,
+                    Body = e.Message,
+                    Headers = new Dictionary<string, string> {
+                        { "Content-Type", "text/plain" }
+                    }
+                };
+            }
+
+            return response;
+        }
+    }
+}
